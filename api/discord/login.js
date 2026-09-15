c6e5791939fb9485c2fffbefc0cd92f2af50c735
nuxidevv <nuxispam@gmail.com>
2026-09-15 17:50:59 +0200

    Delete api/discord directory

--- api/discord/login.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const clientId = process.env.DISCORD_CLIENT_ID;
-  const redirectUri = process.env.DISCORD_REDIRECT_URI;
-  const scope = "identify guilds.join";
-  const url = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${encodeURIComponent(scope)}`;
-  res.redirect(302, url);
-}
--- api/discord/logout.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Set-Cookie", "seek_session=; HttpOnly; Secure; SameSite=Lax; Path=/; Max-Age=0");
-  res.status(200).json({ ok: true });
-}
